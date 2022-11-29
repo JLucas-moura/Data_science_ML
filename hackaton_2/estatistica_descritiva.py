c650fa7d800a4944e29a54d54aeb0cf69fd95f0a
João Lucas Moura <joaolucas.moreira.moura@gmail.com>
2022-11-29 20:22:25 -0300

    Update estatistica_descritiva.py
    pequenas alterações e ajustes nas formulas dos gráficos

--- hackaton_2/estatistica_descritiva.py
+++ hackaton_2/estatistica_descritiva.py
@@ -34,16 +34,29 @@ plt.show()
 
 df_plot
 
-def get_plot(variaveis):
-    for i in variaveis:
-        
-        df_plot = df.groupby([i]).mean().reset_index()
-        df_plot = df_plot.melt(id_vars=i,
+def get_plot(variaveis, x, y):
+
+        df_plot = df.groupby([variaveis]).mean().reset_index()
+        df_plot = df_plot.melt(id_vars=variaveis,
                                value_vars=["BMI", "PhysicalHealth", "MentalHealth", "SleepTime"])
-        sns.barplot(data = df_plot, x="variable", y="value", hue=i)
+        sns.barplot(data = df_plot, x="variable", y="value", hue=variaveis)
+        plt.rcParams['figure.figsize']=(x,y)
         
-    
-get_plot(df.columns)
+
+get_plot('HeartDisease')
+get_plot('AlcoholDrinking')
+get_plot('Stroke')
+get_plot('DiffWalking')
+get_plot('Sex')
+get_plot('AgeCategory', 10, 10)
+get_plot('Race')
+get_plot('Diabetic')
+get_plot('PhysicalActivity')
+get_plot('GenHealth')
+get_plot('Asthma')
+get_plot('KidneyDisease')
+get_plot('SkinCancer')
+
 
 ## Histograma
 def get_hist(variavel, bins):
@@ -54,11 +67,13 @@ def get_hist(variavel, bins):
     plt.xticks(rotation=90)
     plt.show()
 
+# histogramas utilizados
 get_hist("BMI", 40)
 get_hist("PhysicalHealth", 40)
 get_hist("MentalHealth", 30)
 get_hist("SleepTime", 25)
 get_hist("AgeCategory", 13)
+
 
 ## correlação e heatmap das variáveis 
 
